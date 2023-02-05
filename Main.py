bf712d9fb2ca3083a6e8ee77df57cee204d26b1c
Buffahardt <kengelhardt@uni-osnabrueck.de>
2023-02-05 11:46:36 +0100

    New folder Structure

---
+++ Main.py
@@ -0,0 +1,90 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home/home.html')
+
+@app.route('/home/kontakt/')
+def kontakt():
+    return render_template('home/kontakt.html')
+
+@app.route('/home/anleitung/')
+def anleitung():
+    return render_template('home/anleitung.html')
+
+@app.route('/home/hintergrund/')
+def hintergrund():
+    return render_template('home/hintergrund.html')
+
+""" Level 1: Streming Abo beenden """
+#Startseite
+@app.route('/deceptv/')
+def deceptv():
+    return render_template('deceptv/deceptv.html')
+
+@app.route('/deceptv/account/')
+def deceptv_account():
+    return render_template('deceptv/deceptv_account.html')
+
+@app.route('/deceptv/angebote/')
+def deceptv_angebote():
+    return render_template('deceptv/deceptv_angebote.html')
+
+@app.route('/deceptv/datenschutzmanager/')
+def deceptv_datenschutzmanager():
+    return render_template('deceptv/deceptv_datenschutzmanager.html')
+
+@app.route('/deceptv/datenschutrichtlinie/')
+def deceptv_datenschutzrichtlinie():
+    return render_template('deceptv/deceptv_datenschutzrichtlinie.html')
+
+@app.route('/deceptv/erkunden')
+def deceptv_erkunden():
+    return render_template('deceptv/deceptv_erkunden.html')
+
+@app.route('/deceptv/favoriten/')
+def deceptv_favoriten():
+    return render_template('deceptv/deceptv_favoriten.html')
+
+@app.route('/deceptv/feedback/')
+def deceptv_feedback():
+    return render_template('deceptv/deceptv_feedback.html')
+
+@app.route('/deceptv/impressum/')
+def deceptv_impressum():
+    return render_template('deceptv/deceptv_impressum.html')
+
+@app.route('/deceptv/premium/')
+def deceptv_premium():
+    return render_template('deceptv/deceptv_premium.html')
+
+@app.route('/deceptv/service/')
+def deceptv_service():
+    return render_template('deceptv/deceptv_service.html')
+
+@app.route('/deceptv/shop/')
+def deceptv_shop():
+    return render_template('deceptv/deceptv_shop.html')
+    
+@app.route('/deceptv/social/')
+def deceptv_social():
+    return render_template('deceptv/deceptv_social.html')
+
+@app.route('/deceptv/vorschlaege')
+def deceptv_vorschlaege():
+    return render_template('deceptv/deceptv_vorschlaege.html')
+
+#Mitgliedschaft beenden
+@app.route('/deceptv/premium/mein-abo')
+def beenden1():
+    return render_template('deceptv/end/beenden1.html')
+    
+
+
+
+
+
+if __name__ == "__Main__":
+    app.run(debug=True)
